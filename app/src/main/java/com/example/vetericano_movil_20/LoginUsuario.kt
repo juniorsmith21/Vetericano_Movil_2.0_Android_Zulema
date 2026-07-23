cb9780e967811f4395170c7eb1139822d6a2e77f
juniorsmith21 <ordonezjun0@gmail.com>
2026-07-22 20:59:52 -0500

    creacion de proyecto con respectivos roles

---
+++ app/src/main/java/com/example/vetericano_movil_20/LoginUsuario.kt
@@ -0,0 +1,20 @@
+package com.example.vetericano_movil_20
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginUsuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_usuario)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
