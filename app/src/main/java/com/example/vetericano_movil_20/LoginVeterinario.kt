b357fc1ee37500aa5a8ecc4089e37fac53054bc9
Froshi <kg5892868@gmail.com>
2026-07-23 17:56:29 -0500

    feat: create the logic of bottons in loginveterinario and registerveterinario without save data

--- app/src/main/java/com/example/vetericano_movil_20/LoginVeterinario.kt
+++ app/src/main/java/com/example/vetericano_movil_20/LoginVeterinario.kt
@@ -1,20 +1,27 @@
 package com.example.vetericano_movil_20
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vetericano_movil_20.databinding.ActivityLoginVeterinarioBinding
 
 class LoginVeterinario : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginVeterinarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginVeterinarioBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_veterinario)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnRegistrarse.setOnClickListener {
+            val intent = Intent(this, LoginVeterinario::class.java)
         }
     }
 }
--- app/src/main/java/com/example/vetericano_movil_20/RegisterVeterinario.kt
+++ app/src/main/java/com/example/vetericano_movil_20/RegisterVeterinario.kt
@@ -1,20 +1,27 @@
 package com.example.vetericano_movil_20
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vetericano_movil_20.databinding.ActivityRegisterVeterinarioBinding
 
 class RegisterVeterinario : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterVeterinarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterVeterinarioBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register_veterinario)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnLoginofregister.setOnClickListener {
+            val intent = Intent(this, LoginVeterinario::class.java)
         }
     }
 }
