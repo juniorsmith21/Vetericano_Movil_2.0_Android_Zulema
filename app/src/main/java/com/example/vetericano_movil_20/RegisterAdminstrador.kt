d823f47372fc27889b39b0632aa95b0a4076b657
JDquintero1002 <jdquintero@unicauca.edu.co>
2026-07-23 16:43:10 -0500

    lgin y registro, creación de la interfaz

--- app/src/main/java/com/example/vetericano_movil_20/RegisterAdminstrador.kt
+++ app/src/main/java/com/example/vetericano_movil_20/RegisterAdminstrador.kt
@@ -1,20 +1,28 @@
 package com.example.vetericano_movil_20
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vetericano_movil_20.databinding.ActivityMainBinding
+import com.example.vetericano_movil_20.databinding.ActivityRegisterAdminstradorBinding
 
 class RegisterAdminstrador : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterAdminstradorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register_adminstrador)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterAdminstradorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegistrar.setOnClickListener {
+            val intent = Intent(this, RegisterAdminstrador::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "cuenta registrada", Toast.LENGTH_SHORT).show()
         }
+        binding.b
     }
 }
