0a56d21f085264d8fdbdf9764e199af5cb0d2c4e
Jubelit zapata <gjubelit@gmail.com>
2026-07-23 16:28:59 -0500

    Creacion de el registro

--- app/src/main/java/com/example/vetericano_movil_20/RegisterUsuario.kt
+++ app/src/main/java/com/example/vetericano_movil_20/RegisterUsuario.kt
@@ -1,20 +1,35 @@
 package com.example.vetericano_movil_20
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vetericano_movil_20.databinding.ActivityMainBinding
+import com.example.vetericano_movil_20.databinding.ActivityRegisterUsuarioBinding
 
 class RegisterUsuario : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterUsuarioBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register_usuario)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityRegisterUsuarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegistro.setOnClickListener {
+
+
         }
+
+            binding.btnRegresar.setOnClickListener {
+                val intent= Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
     }
 }
